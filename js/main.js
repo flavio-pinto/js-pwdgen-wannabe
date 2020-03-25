906a35fb7bc9ab7bcf30e3ed361cd3ade610553a
Flavio Pinto <flaviopnt@hotmail.it>
2020-03-25 16:17:06 +0100

    Password Generator completed

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// 1 - Richiesta nome
+var name = prompt('Qual è il tuo nome di battesimo?');
+console.log(name);
+
+// 2 - Richiesta cognome
+var surname = prompt('Qual è il tuo cognome?');
+console.log(surname);
+
+// 3 - Richiesta colore preferito
+var color = prompt('Qual è il tuo colore preferito?');
+console.log(color);
+
+// 4 - Generare password
+document.getElementById('your-pw-is').innerHTML = 'La tua password: ';
+document.getElementById('password').innerHTML =  name + surname + color + '20';
